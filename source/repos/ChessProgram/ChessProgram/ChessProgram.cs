a036548b9c11ffa191ec6471ff7c09cc59c082c4
rohithm123 <51744983+rohithm123@users.noreply.github.com>
2020-08-24 14:29:47 +0530

    ChessProgramAssignment

---
+++ source/repos/ChessProgram/ChessProgram/ChessProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ChessDashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, remainder;
+            for (i = 1; i <= 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    remainder = 1; 
+                }
+                else
+                {
+                    remainder = 0; 
+                }
+                for (j = 1; j <= 8; j++)
+                {
+                    if (j % 2 == remainder)
+                    {
+                        Console.Write("WW");
+                        Console.Write("|");
+                    }
+                    else
+                    {
+                        Console.Write("BB");
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
